9dc5995fc8d0c32eb6a455128fd2b5c44aa87754
Quang Hai <lqhai2021@gmail.com>
2023-11-20 16:57:37 +0700

    create angular app

--- fontent/src/app/app.component.spec.ts
+++ fontent/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fontend'`, () => {
+  it(`should have as title 'fontent'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fontend');
+    expect(app.title).toEqual('fontent');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('fontent app is running!');
   });
 });
